b4bce361d53f56a3394273ff7e89a42219f512d5
rzndluisa <rzndluisa@gmail.com>
2024-10-07 10:19:18 -0300

    criação da função

---
+++ script.js
@@ -0,0 +1,11 @@
+let numeroSecreto;
+let tentativas = 0;
+
+function gerarNumeroAleatorio() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            numeroSecreto
+        })
+    }
+    )
+}
