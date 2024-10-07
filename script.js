2226a0a9d0b371eaa046e8b2cd44db628860bd23
rzndluisa <rzndluisa@gmail.com>
2024-10-07 10:40:28 -0300

    criação da mensagem de erro

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ let tentativas = 0;
 function gerarNumeroAleatorio() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            numeroSecreto = Math.floor(Math.random()*100)+1;
+            numeroSecreto = Math.floor(Math.random() * 100) + 1;
             resolve(numeroSecreto);
         }, 1000)
     });
 }
-function verificarPalpite(palpite){
-    
+function verificarPalpite(palpite) {
+    return new Promise((resolve, reject) => {
+        tentativas++;
+        if(palpite < 1 || palpite > 100) {
+            reject("Por favor, digite um número de 1 a 100.")
+            
+        }
+    });
 }
